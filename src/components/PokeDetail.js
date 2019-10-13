60f65cc07eb2559a7b0552db187dad65a81ed14d
unknown <brandonellis@gmail.com>
2019-10-12 23:33:03 -0500

    convert weight to pounds and height to feet/inches

--- src/components/PokeDetail.js
+++ src/components/PokeDetail.js
@@ -35,13 +35,19 @@ class PokeDetail extends Component {
     pokeId = pokeId.toString();
     pokeId = pokeId.padStart(3, '0'); 
 
-    const {name, weight, height} = this.state.pokeData
+    let {name, weight, height} = this.state.pokeData
+    let realFeet = ((height/10)*39.370) / 12
+    let feet = Math.floor(realFeet);
+    let inches = Math.round((realFeet - feet) * 12);
+
+    let pounds  = weight * 0.2204622622
+    pounds = Math.round(10*pounds)/10;  
 
     return ( 
     <div className="flex-1 text-gray-700 text-center bg-gray-400 px-4 py-2 m-2">
         <h1 className="text-6xl uppercase">{name}</h1>
         <img className="object-contain h-64 w-full" src={'https://assets.pokemon.com/assets/cms2/img/pokedex/full/' + pokeId + '.png'}></img>
-        Weight: {weight} <br></br> Height: {height}
+        Weight: {pounds}lbs <br></br> Height: {feet}&prime; {inches}&Prime;
     </div>
   //convert decimetre to feet
     
